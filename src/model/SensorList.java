ec97c25468a37b90c63819f468b06bfa8f41607b
Rodion Podorozhny <rp31@txstate.edu>
2020-07-24 14:43:50 -0500

    removed sensors 1 to 3

--- src/model/SensorList.java
+++ src/model/SensorList.java
@@ -3,9 +3,6 @@ package model;
 public class SensorList {
 	public void print(){
 		System.out.println("Sensor list");
-		System.out.println("2_RP17 Sensor 1");
-		System.out.println("2_RP17 Sensor 2");
-		System.out.println("2_RP17 Sensor 3");
 		System.out.println("2_RP17 Sensor 4");
 		System.out.println("2_RP17 Sensor 5");
 		System.out.println("2_RP17 Sensor 6");	
